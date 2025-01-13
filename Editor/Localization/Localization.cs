d1e779142c64fbc5c2a0af5f6bd3482dc30900ed
kesera2 <38027941+kesera2@users.noreply.github.com>
2025-01-13 15:22:35 +0900

    Add the localization(wip)

---
+++ Editor/Localization/Localization.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine;
+
+namespace dev.kesera2.physbone_extractor
+{
+    public static class Localization
+    {
+        private const string LocalizationPathGuid = "603be7a353df47e0b812ae8f1f1b2a28";
+        private static readonly string LocalizationPathRoot = AssetDatabase.GUIDToAssetPath(LocalizationPathGuid);
+        private static Dictionary<string, string> _translations;
+        private static string _selectedLanguage; // デフォルト言語
+
+        public static void LoadLocalization(string language)
+        {
+            Debug.Log("language: " + language);
+            string filePath = GetFilePath(language);
+
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                _translations = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonData);
+                _selectedLanguage = language;
+            }
+            else
+            {
+                Debug.LogError($"Localization file not found: {filePath}");
+            }
+        }
+        
+        public static string GetTranslation(string key)
+        {
+            if (_translations != null && _translations.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+            return key; // デフォルトとしてキーを返す
+        }
+        
+        public static void SetLanguage(string language)
+        {
+            LoadLocalization(language);
+        }
+        
+        private static string GetFilePath(string language)
+        {
+            return $"{LocalizationPathRoot}/{language}.json";
+        }
+    }
+}
--- Editor/PhysBoneExtractor.cs
+++ Editor/PhysBoneExtractor.cs
@@ -3,168 +3,183 @@ using UnityEditor;
 using UnityEngine;
 using VRC.SDK3.Dynamics.PhysBone.Components;
 
-public class PhysBoneCopierWindow : EditorWindow
+namespace dev.kesera2.physbone_extractor
 {
-    [SerializeField] private GameObject prefabRoot; // 元のGameObject
-    [SerializeField] private GameObject searchRoot; // 探索するGameObject
-    [SerializeField] private bool isDeleteEnabled = true;
-    private GameObject _avatarDynamics;
-
-    [MenuItem("Tools/kesera2/PhysBone Extractor")]
-    public static void ShowWindow()
+    public class PhysBoneExtractor : EditorWindow
     {
-        GetWindow<PhysBoneCopierWindow>("Phys Bone Copier");
-    }
+        [SerializeField] private GameObject prefabRoot; // 元のGameObject
+        [SerializeField] private GameObject searchRoot; // 探索するGameObject
+        [SerializeField] private bool isDeleteEnabled = true;
+        private string[] languageOptions = { "ja-jp", "en-us" };
+        private int _selectedLanguage = 0;
+        private GameObject _avatarDynamics;
 
-    private void OnGUI()
-    {
-        GUILayout.Label("Select GameObject", EditorStyles.boldLabel);
-        prefabRoot =
-            (GameObject)EditorGUILayout.ObjectField("Prefab Root GameObject", prefabRoot, typeof(GameObject), true);
-        searchRoot =
-            (GameObject)EditorGUILayout.ObjectField("Search Root GameObject", searchRoot, typeof(GameObject), true);
-        isDeleteEnabled = EditorGUILayout.Toggle("オリジナルのコンポーネントを削除する", isDeleteEnabled);
-        if (GUILayout.Button("Copy VRC Phys Bones"))
+        [MenuItem("Tools/kesera2/PhysBone Extractor")]
+        public static void ShowWindow()
         {
-            if (prefabRoot != null && searchRoot != null)
+            GetWindow<PhysBoneExtractor>("PhysBone Extractor");
+        }
+        
+        private void ShowSelectLanguage()
+        {
+            var selectedLanguage = EditorGUILayout.Popup("Select Language", _selectedLanguage, languageOptions);
+            if (_selectedLanguage != selectedLanguage) Localization.LoadLocalization(languageOptions[selectedLanguage]);
+            _selectedLanguage = selectedLanguage;
+        }
+
+        private void OnGUI()
+        {
+            // 言語選択ポップアップ
+            ShowSelectLanguage();
+            EditorGUILayout.LabelField(Localization.GetTranslation("test"));   
+            GUILayout.Label("Select GameObject", EditorStyles.boldLabel);
+            prefabRoot =
+                (GameObject)EditorGUILayout.ObjectField("Prefab Root GameObject", prefabRoot, typeof(GameObject), true);
+            searchRoot =
+                (GameObject)EditorGUILayout.ObjectField("Search Root GameObject", searchRoot, typeof(GameObject), true);
+            isDeleteEnabled = EditorGUILayout.Toggle("オリジナルのコンポーネントを削除する", isDeleteEnabled);
+            if (GUILayout.Button("Copy VRC Phys Bones"))
             {
-                CreateAvatarDynamics();
-                var hasPhysbone = CopyPhysBones();
-                var hasPhysboneCollider = CopyPhysBoneColliders();
-                if (!(hasPhysbone || hasPhysboneCollider))
-                    // Destroy the GameObject of AvatarDynamics if there is no target components.
-                    DestroyImmediate(_avatarDynamics);
+                if (prefabRoot != null && searchRoot != null)
+                {
+                    CreateAvatarDynamics();
+                    var hasPhysbone = CopyPhysBones();
+                    var hasPhysboneCollider = CopyPhysBoneColliders();
+                    if (!(hasPhysbone || hasPhysboneCollider))
+                        // Destroy the GameObject of AvatarDynamics if there is no target components.
+                        DestroyImmediate(_avatarDynamics);
+                }
+                else
+                {
+                    Debug.LogWarning("Please assign both Target and Search Root GameObjects.");
+                }
             }
-            else
+        }
+
+        private void CreateAvatarDynamics()
+        {
+            // Create PB GameObject under the target GameObject
+            _avatarDynamics = new GameObject("AvatarDynamics");
+            _avatarDynamics.transform.SetParent(prefabRoot.transform);
+        }
+
+        private bool CopyPhysBones()
+        {
+            var pbParent = new GameObject("PB");
+            pbParent.transform.SetParent(_avatarDynamics.transform);
+
+            var vrcPhysBones = new List<VRCPhysBone>(searchRoot.GetComponentsInChildren<VRCPhysBone>());
+
+            foreach (var sourcePhysBone in vrcPhysBones)
             {
-                Debug.LogWarning("Please assign both Target and Search Root GameObjects.");
+                var sourceTransform = sourcePhysBone.transform;
+                // Create new GameObject and copy component
+                var newPhysBone = new GameObject(sourceTransform.name);
+                newPhysBone.transform.SetParent(pbParent.transform);
+
+                // Copy the VRC Phys Bone component
+                var newVrcPhysBone = newPhysBone.AddComponent<VRCPhysBone>();
+                CopyVRCPhysBone(sourcePhysBone, newVrcPhysBone);
+
+                // Set the Root Transform
+                if (!newVrcPhysBone.rootTransform) newVrcPhysBone.rootTransform = newPhysBone.transform;
+
+                // Remove original VRC Phys Bone component
+                if (isDeleteEnabled) DestroyImmediate(sourcePhysBone);
             }
+
+            return vrcPhysBones.Count > 0;
         }
-    }
 
-    private void CreateAvatarDynamics()
-    {
-        // Create PB GameObject under the target GameObject
-        _avatarDynamics = new GameObject("AvatarDynamics");
-        _avatarDynamics.transform.SetParent(prefabRoot.transform);
-    }
-
-    private bool CopyPhysBones()
-    {
-        var pbParent = new GameObject("PB");
-        pbParent.transform.SetParent(_avatarDynamics.transform);
-
-        var vrcPhysBones = new List<VRCPhysBone>(searchRoot.GetComponentsInChildren<VRCPhysBone>());
-
-        foreach (var sourcePhysBone in vrcPhysBones)
+        private bool CopyPhysBoneColliders()
         {
-            var sourceTransform = sourcePhysBone.transform;
-            // Create new GameObject and copy component
-            var newPhysBone = new GameObject(sourceTransform.name);
-            newPhysBone.transform.SetParent(pbParent.transform);
+            var pbColliderParent = new GameObject("PBCollider");
+            pbColliderParent.transform.SetParent(_avatarDynamics.transform);
+            var vrcPhysboneColliders =
+                new List<VRCPhysBoneCollider>(searchRoot.GetComponentsInChildren<VRCPhysBoneCollider>());
+            foreach (var sourcePbCollider in vrcPhysboneColliders)
+            {
+                var sourceTransform = sourcePbCollider.transform;
+                // Create new GameObject and copy component
+                var newCollider = new GameObject(sourceTransform.name);
+                newCollider.transform.SetParent(pbColliderParent.transform);
 
-            // Copy the VRC Phys Bone component
-            var newVrcPhysBone = newPhysBone.AddComponent<VRCPhysBone>();
-            CopyVRCPhysBone(sourcePhysBone, newVrcPhysBone);
+                // Copy the VRC Phys Bone Collider component
+                var destPbCollider = newCollider.AddComponent<VRCPhysBoneCollider>();
+                CopyVRCPhysboneCollider(sourcePbCollider, destPbCollider);
 
-            // Set the Root Transform
-            if (!newVrcPhysBone.rootTransform) newVrcPhysBone.rootTransform = newPhysBone.transform;
+                // Set the Root Transform
+                if (!destPbCollider.rootTransform) destPbCollider.rootTransform = newCollider.transform;
 
-            // Remove original VRC Phys Bone component
-            if (isDeleteEnabled) DestroyImmediate(sourcePhysBone);
+                // Remove original VRC Phys Bone Collider component
+                if (isDeleteEnabled) DestroyImmediate(sourcePbCollider);
+            }
+
+            return vrcPhysboneColliders.Count > 0;
         }
 
-        return vrcPhysBones.Count > 0;
-    }
-
-    private bool CopyPhysBoneColliders()
-    {
-        var pbColliderParent = new GameObject("PBCollider");
-        pbColliderParent.transform.SetParent(_avatarDynamics.transform);
-        var vrcPhysboneColliders =
-            new List<VRCPhysBoneCollider>(searchRoot.GetComponentsInChildren<VRCPhysBoneCollider>());
-        foreach (var sourcePbCollider in vrcPhysboneColliders)
+        private void CopyVRCPhysBone(VRCPhysBone source, VRCPhysBone destination)
         {
-            var sourceTransform = sourcePbCollider.transform;
-            // Create new GameObject and copy component
-            var newCollider = new GameObject(sourceTransform.name);
-            newCollider.transform.SetParent(pbColliderParent.transform);
-
-            // Copy the VRC Phys Bone Collider component
-            var destPbCollider = newCollider.AddComponent<VRCPhysBoneCollider>();
-            CopyVRCPhysboneCollider(sourcePbCollider, destPbCollider);
-
-            // Set the Root Transform
-            if (!destPbCollider.rootTransform) destPbCollider.rootTransform = newCollider.transform;
-
-            // Remove original VRC Phys Bone Collider component
-            if (isDeleteEnabled) DestroyImmediate(sourcePbCollider);
+            // Copy properties from source to destination
+            destination.name = source.name;
+            destination.enabled = source.enabled;
+            // Transforms
+            destination.rootTransform = source.rootTransform;
+            destination.ignoreTransforms = source.ignoreTransforms;
+            destination.endpointPosition = source.endpointPosition;
+            destination.multiChildType = source.multiChildType;
+            // Forces
+            destination.integrationType = source.integrationType;
+            destination.pull = source.pull;
+            destination.spring = source.spring;
+            destination.stiffness = source.stiffness;
+            destination.gravity = source.gravity;
+            destination.gravityCurve = source.gravityCurve;
+            // Limits
+            destination.limitType = source.limitType;
+            destination.maxAngleX = source.maxAngleX;
+            destination.maxAngleZ = source.maxAngleZ;
+            destination.maxAngleXCurve = source.maxAngleXCurve;
+            destination.maxAngleZCurve = source.maxAngleZCurve;
+            destination.limitRotation = source.limitRotation;
+            destination.limitOpacity = source.limitOpacity;
+            destination.limitRotationXCurve = source.limitRotationXCurve;
+            destination.limitRotationYCurve = source.limitRotationYCurve;
+            destination.limitRotationZCurve = source.limitRotationZCurve;
+            // Colliders
+            destination.radius = source.radius;
+            destination.allowCollision = source.allowCollision;
+            destination.colliders = source.colliders;
+            // Stretch & Squish
+            destination.maxSquish = source.maxSquish;
+            // Grab & Pose
+            destination.allowGrabbing = source.allowGrabbing;
+            destination.allowPosing = source.allowPosing;
+            destination.grabMovement = source.grabMovement;
+            destination.snapToHand = source.snapToHand;
+            // Options
+            destination.parameter = source.parameter;
+            destination.isAnimated = source.isAnimated;
+            destination.resetWhenDisabled = source.resetWhenDisabled;
+            destination.immobileType = source.immobileType;
         }
 
-        return vrcPhysboneColliders.Count > 0;
-    }
-
-    private void CopyVRCPhysBone(VRCPhysBone source, VRCPhysBone destination)
-    {
-        // Copy properties from source to destination
-        destination.name = source.name;
-        destination.enabled = source.enabled;
-        // Transforms
-        destination.rootTransform = source.rootTransform;
-        destination.ignoreTransforms = source.ignoreTransforms;
-        destination.endpointPosition = source.endpointPosition;
-        destination.multiChildType = source.multiChildType;
-        // Forces
-        destination.integrationType = source.integrationType;
-        destination.pull = source.pull;
-        destination.spring = source.spring;
-        destination.stiffness = source.stiffness;
-        destination.gravity = source.gravity;
-        destination.gravityCurve = source.gravityCurve;
-        // Limits
-        destination.limitType = source.limitType;
-        destination.maxAngleX = source.maxAngleX;
-        destination.maxAngleZ = source.maxAngleZ;
-        destination.maxAngleXCurve = source.maxAngleXCurve;
-        destination.maxAngleZCurve = source.maxAngleZCurve;
-        destination.limitRotation = source.limitRotation;
-        destination.limitOpacity = source.limitOpacity;
-        destination.limitRotationXCurve = source.limitRotationXCurve;
-        destination.limitRotationYCurve = source.limitRotationYCurve;
-        destination.limitRotationZCurve = source.limitRotationZCurve;
-        // Colliders
-        destination.radius = source.radius;
-        destination.allowCollision = source.allowCollision;
-        destination.colliders = source.colliders;
-        // Stretch & Squish
-        destination.maxSquish = source.maxSquish;
-        // Grab & Pose
-        destination.allowGrabbing = source.allowGrabbing;
-        destination.allowPosing = source.allowPosing;
-        destination.grabMovement = source.grabMovement;
-        destination.snapToHand = source.snapToHand;
-        // Options
-        destination.parameter = source.parameter;
-        destination.isAnimated = source.isAnimated;
-        destination.resetWhenDisabled = source.resetWhenDisabled;
-        destination.immobileType = source.immobileType;
-    }
-
-    private void CopyVRCPhysboneCollider(VRCPhysBoneCollider source, VRCPhysBoneCollider destination)
-    {
-        // Copy properties from source to destination
-        destination.name = source.name;
-        destination.enabled = source.enabled;
-        destination.rootTransform = source.rootTransform;
-        destination.shapeType = source.shapeType;
-        destination.insideBounds = source.insideBounds;
-        destination.radius = source.radius;
-        destination.height = source.height;
-        destination.position = source.position;
-        destination.rotation = source.rotation;
-        destination.bonesAsSpheres = source.bonesAsSpheres;
-        destination.isGlobalCollider = source.isGlobalCollider;
-        destination.playerId = source.playerId;
-        destination.shape = source.shape;
+        private void CopyVRCPhysboneCollider(VRCPhysBoneCollider source, VRCPhysBoneCollider destination)
+        {
+            // Copy properties from source to destination
+            destination.name = source.name;
+            destination.enabled = source.enabled;
+            destination.rootTransform = source.rootTransform;
+            destination.shapeType = source.shapeType;
+            destination.insideBounds = source.insideBounds;
+            destination.radius = source.radius;
+            destination.height = source.height;
+            destination.position = source.position;
+            destination.rotation = source.rotation;
+            destination.bonesAsSpheres = source.bonesAsSpheres;
+            destination.isGlobalCollider = source.isGlobalCollider;
+            destination.playerId = source.playerId;
+            destination.shape = source.shape;
+        }
     }
 }
